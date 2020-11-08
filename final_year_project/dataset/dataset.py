61c171eb429dc798aa7a2983b9f2f31d1946eb4c
Eric Fan <50669888+windshadow233@users.noreply.github.com>
2020-11-08 18:24:31 +0800

    写了个数据集

---
+++ final_year_project/dataset/dataset.py
@@ -0,0 +1,61 @@
+from torch.utils.data import Dataset
+import torchvision
+from torchvision.datasets import ImageFolder
+from torchvision import transforms
+import matplotlib.pyplot as plt
+import torch
+import numpy as np
+
+data_transform = transforms.Compose([transforms.Grayscale(1),
+                                     transforms.Resize((64, 64), 2),
+                                     transforms.RandomHorizontalFlip(p=0.5),
+                                     transforms.ToTensor(),
+                                     transforms.Normalize(mean=[0.5], std=[0.5])
+                                     ])
+
+
+class LfwDataset(Dataset):
+    def __init__(self, root_dir=None):
+        if root_dir:
+            self.data = ImageFolder(root_dir)
+        else:
+            self.data = None
+
+    def __getitem__(self, index):
+        """
+
+        :param index: 表示数据的index
+        :return: 返回两张图片和label，label为1表示同一个人，为0表示非同一个人
+        """
+        img1 = self.data[2 * index][0]
+        img2 = self.data[2 * index + 1][0]
+        label = self.data[2 * index][1]
+        return data_transform(img1), data_transform(img2), torch.tensor([label], dtype=torch.float32)
+
+    def __add__(self, other):
+        lfw_dataset = LfwDataset()
+        lfw_dataset.data = self.data + other.data
+        return lfw_dataset
+
+    def __len__(self):
+        return len(self.data) // 2
+
+
+def imshow(img, text=None):
+    npimg = img.numpy()
+    plt.axis("off")
+    if text:
+        plt.text(75, 8, text, style='italic', fontweight='bold',
+                 bbox={'facecolor': 'white', 'alpha': 0.8, 'pad': 10})
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+def visualize(data_loader, batch_size=8):
+    example_batch = next(iter(data_loader))
+    concatenated = torch.cat((example_batch[0], example_batch[1]), 0)
+    imshow(torchvision.utils.make_grid(concatenated, nrow=batch_size))
+    print(example_batch[2].numpy())
+
+
+
