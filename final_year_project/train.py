e034f12c8b32a1f222451e6e82899242fe6abb3f
Camille7777 <44392324+Camille7777@users.noreply.github.com>
2020-12-05 00:50:36 +0800

    10-fold forever bug

--- final_year_project/train.py
+++ final_year_project/train.py
@@ -10,11 +10,11 @@ from model.cnn_model import CNN, ContrastiveLoss
 from dataset.dataset import LfwDataset
 import numpy as np
 
-train_number_epochs = 20
+train_number_epochs = 2
 
 
 class Trainer:
-    def __init__(self, model, learning_rate, batch_size, use_cuda=True):
+    def __init__(self, model, learning_rate=1e-2, batch_size=32, use_cuda=True):
         self.model = model()
         self.device = torch.device('cuda:0') if use_cuda and torch.cuda.is_available() else torch.device('cpu')
         self.batch_size = batch_size
@@ -26,74 +26,6 @@ class Trainer:
         self.counter = []
         self.loss_history = []
         self.iteration_number = 0
-
-    def get_10_folder_data(self, index):
-        valid_dataset = self.datasets[index]
-        train_dataset = sum([self.datasets[i] for i in range(10) if i != index])
-        return valid_dataset, train_dataset
-
-    '''def k_folder_iteration(self, num_epochs):
-        train_loss_sum, valid_loss_sum = 0, 0
-        train_acc_sum, valid_acc_sum = 0, 0
-
-        for k in range(10):
-            valid, train = self.get_10_folder_data(k)
-            valid_loader = DataLoader(valid, batch_size=self.batch_size, shuffle=True)
-            train_loader = DataLoader(train, batch_size=self.batch_size, shuffle=True)
-            train_ls = self.train_k(train_loader, num_epochs)
-            valid_ls = self.train_k(valid_loader, num_epochs)
-
-            print('*' * 25, 'Cross', k + 1, '*' * 25)
-            print('train_loss:%.6f' % train_ls[-1][0], 'train_acc:%.4f\n' % valid_ls[-1][1],
-                  'valid loss:%.6f' % valid_ls[-1][0], 'valid_acc:%.4f' % valid_ls[-1][1])
-            train_loss_sum += train_ls[-1][0]
-            valid_loss_sum += valid_ls[-1][0]
-            train_acc_sum += train_ls[-1][1]
-            valid_acc_sum += valid_ls[-1][1]
-        print('#' * 10, 'Final 10-cross validation result', '#' * 10)
-
-        print('train_loss_sum:%.4f' % (train_loss_sum / 10), 'train_acc_sum:%.4f\n' % (train_acc_sum / 10),
-              'valid_loss_sum:%.4f' % (valid_loss_sum / 10), 'valid_acc_sum:%.4f' % (valid_acc_sum / 10))'''
-
-    '''
-    def train_k(self, valid_dataset, train_dataset, train_num_epochs):
-        train_ls, valid_ls = [], []
-        self.model.cuda()
-        train_dataloader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
-        valid_dataloader = DataLoader(valid_dataset, batch_size=self.batch_size, shuffle=True)
-        for epoch in range(0, train_num_epochs):
-            for i, data in enumerate(train_dataloader, 0):
-                img1, img2, img1_soft_biometrics, img2_soft_biometrics, train_label = data
-                img1, img2, img1_soft_biometrics, img2_soft_biometrics, train_label = img1.to(self.device), \
-                                                                                      img2.to(self.device), \
-                                                                                      img1_soft_biometrics.to(
-                                                                                          self.device), \
-                                                                                      img2_soft_biometrics.to(
-                                                                                          self.device), \
-                                                                                      train_label.to(
-                                                                                          self.device)
-                output1, output2 = self.model(img1, img2)
-                loss_contrastive = self.loss_fcn(output1, output2, train_label)
-                self.optimizer.zero_grad()
-                loss_contrastive.backward()
-                self.optimizer.step()
-                train_ls.append(loss_contrastive.item())
-                
-        for i, data in enumerate(valid_dataloader, 0):
-            img1, img2, img1_soft_biometrics, img2_soft_biometrics, train_label = data
-            img1, img2, img1_soft_biometrics, img2_soft_biometrics, train_label = img1.to(self.device), \
-                                                                                  img2.to(self.device), \
-                                                                                  img1_soft_biometrics.to(self.device), \
-                                                                                  img2_soft_biometrics.to(self.device), \
-                                                                                  train_label.to(self.device)
-            output1, output2 = self.model(img1, img2)
-            loss_contrastive = self.loss_fcn(output1, output2, train_label)
-            self.optimizer.zero_grad()
-            loss_contrastive.backward()
-            self.optimizer.step()
-            valid_ls.append(loss_contrastive.item())
-        return train_ls, valid_ls
-    '''
 
     def train(self):
         self.model.cuda()
@@ -128,6 +60,19 @@ class Trainer:
         show_plot(self.counter, self.loss_history)
 
 
+@torch.no_grad()
+def test_model(model, test_loader: DataLoader):
+    model.eval()
+    count = 0
+    for img1, img2, _, _, lb in test_loader:
+        img1, img2, lb = img1.to(t.device), img2.to(t.device), lb.to(t.device)
+        out1, out2 = model(img1, img2)
+        dist = F.pairwise_distance(out1, out2)
+        p = (dist < 1).int()
+        count += (p == lb).float().sum()
+    return count.item() / len(test_loader.dataset)
+
+
 def imshow(img, text=None, should_save=False):
     npimg = img.numpy()
     plt.axis("off")
@@ -145,7 +90,4 @@ def show_plot(iteration, loss):
 t = Trainer(CNN, learning_rate=1e-2, batch_size=32, use_cuda=True)
 t.train()
 
-'''loss_func = nn.CrossEntropyLoss()
-t = Trainer(CNN, learning_rate=1e-2, weight_decay=1e-1, batch_size=64, use_cuda=True)
-t.k_folder_iteration(3)'''
 
---
+++ final_year_project/train_10_fold_cross_validation.py
@@ -0,0 +1,105 @@
+import torch
+from torch.optim import Adam, SGD
+from torch.utils.data import DataLoader
+import torch.nn as nn
+from torch.nn import functional as F
+import tqdm
+import os
+
+from model.cnn_model import CNN, ContrastiveLoss
+from dataset.dataset import LfwDataset
+
+train_number_epochs = 1
+
+
+class Trainer:
+    def __init__(self, model, learning_rate=1e-2, batch_size=32, use_cuda=True):
+        self.model = model()
+        self.device = torch.device('cuda:0') if use_cuda and torch.cuda.is_available() else torch.device('cpu')
+        self.batch_size = batch_size
+        self.loss_fcn = ContrastiveLoss()
+        self.optimizer = SGD(self.model.parameters(), lr=learning_rate)
+        self.data_root = 'dataset/lfw_cropped/split_data/train'
+        self.datasets = [LfwDataset(os.path.join(self.data_root, f'0{i}'))
+                         for i in tqdm.tqdm(range(1, 11), desc='loading data...')]
+        self.counter = []
+        self.loss_history = []
+        self.iteration_number = 0
+        self.loss_func = nn.CrossEntropyLoss()
+
+    # separate for train dataset and valid dataset
+    def get_10_fold_data(self, index):
+        valid_dataset = self.datasets[index]
+        train_dataset = sum([self.datasets[i] for i in range(10) if i != index])
+        return valid_dataset, train_dataset
+
+    def k_fold_iteration(self, num_epochs):
+        train_loss_sum, valid_loss_sum = 0, 0
+        train_acc_sum, valid_acc_sum = 0, 0
+
+        for k in range(10):
+            valid, train = self.get_10_fold_data(k)
+            #valid_loader = DataLoader(valid, batch_size=self.batch_size, shuffle=True)
+            #train_loader = DataLoader(train, batch_size=self.batch_size, shuffle=True)
+            train_ls = self.train_k(valid, train, num_epochs)
+            valid_ls = self.train_k(valid, train, num_epochs)
+
+            print('*' * 25, 'Number ', k + 1, ' fold', '*' * 25)
+            print('train_loss:%.4f' % train_ls[-1][0], 'train_acc:%.4f\n' % train_ls[-1][1],
+                  'valid loss:%.4f' % valid_ls[-1][0], 'valid_acc:%.4f' % valid_ls[-1][1])
+            train_loss_sum += train_ls[-1][0]
+            valid_loss_sum += valid_ls[-1][0]
+            train_acc_sum += train_ls[-1][1]
+            valid_acc_sum += valid_ls[-1][1]
+        print('#' * 10, 'Final 10-fold cross validation result', '#' * 10)
+
+        print('train_loss_sum:%.4f' % (train_loss_sum / 10), 'train_acc_sum:%.4f\n' % (train_acc_sum / 10),
+              'valid_loss_sum:%.4f' % (valid_loss_sum / 10), 'valid_acc_sum:%.4f' % (valid_acc_sum / 10))
+
+    def train_k(self, valid_dataset, train_dataset, train_num_epochs):
+        train_ls, valid_ls = [], []
+        self.model.cuda()
+        train_dataloader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
+        valid_dataloader = DataLoader(valid_dataset, batch_size=self.batch_size, shuffle=True)
+        for epoch in range(0, train_num_epochs):
+            for i, data in enumerate(train_dataloader, 0):
+                img1, img2,  _, _, train_label = data
+                img1, img2, train_label = img1.to(self.device), img2.to(self.device), train_label.to(self.device)
+
+                output1, output2 = self.model(img1, img2)
+                loss_contrastive = self.loss_fcn(output1, output2, train_label)
+                self.optimizer.zero_grad()
+                loss_contrastive.backward()
+                self.optimizer.step()
+                train_dist = F.pairwise_distance(output1, output2)
+                train_predict_lb = (train_dist < 1).int()
+                train_accurancy = (train_predict_lb == train_label).float().mean().item()
+                train_ls.append(self.res(loss_contrastive.item(), train_accurancy))
+
+            for j, data1 in enumerate(valid_dataloader, 0):
+                self.model.eval()
+                img01, img02, _, _, valid_label = data1
+                img01, img02, valid_label = img1.to(self.device), img2.to(self.device), train_label.to(self.device)
+                count = 0
+
+                output1, output2 = self.model(img1, img2)
+                loss_contrastive = self.loss_fcn(output1, output2, train_label)
+                self.optimizer.zero_grad()
+                loss_contrastive.backward()
+                self.optimizer.step()
+                valid_dist = F.pairwise_distance(output1, output2)
+                valid_predict_lb = (valid_dist < 1).int()
+                count += (valid_predict_lb == valid_label).float().sum()
+                valid_accurancy = count.item() / len(valid_dataloader.dataset)
+                valid_ls.append(self.res(loss_contrastive.item(), valid_accurancy))
+
+        return train_ls, valid_ls
+
+    def res(self, x, y):
+        return x, y
+
+
+t = Trainer(CNN, learning_rate=1e-2, batch_size=32, use_cuda=True)
+t.k_fold_iteration(1)
+
+
