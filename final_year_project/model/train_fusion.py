7cbea914a40c704faac5e84dfb4bfcd4d192cf7b
Camille7777 <44392324+Camille7777@users.noreply.github.com>
2020-12-17 16:29:05 +0800

    Backup

--- final_year_project/model/train_fusion.py
+++ final_year_project/model/train_fusion.py
@@ -6,11 +6,18 @@ def fusion_simple_append(output, soft_bio):
     return result
 
 
+def fusion_kronecker_product(output, soft_bio):
+    result = torch.einsum("ab,ac->abc", output, soft_bio)
+    result = result.view(output.size(0), output.size(1) * soft_bio.size(1))
+    return result
+
+
 def fusion_none(*args):
     return args[0]
 
 
-fusion_dict = {"append": fusion_simple_append}
+fusion_dict = {"append": fusion_simple_append,
+               "kronecker": fusion_kronecker_product}
 
 
 def get_fusion_result(method, output, soft_bio):
--- final_year_project/train.py
+++ final_year_project/train.py
@@ -5,6 +5,10 @@ import torch.nn as nn
 from torch.nn import functional as F
 import tqdm
 import os
+import random
+import numpy as np
+import pandas as pd
+
 
 from model.cnn_model import DSASN, ContrastiveLoss
 from dataset.dataset import LfwDataset
@@ -18,13 +22,19 @@ class Trainer:
         self.loss_fcn = ContrastiveLoss()
         self.optimizer = Adam
         self.lr = learning_rate
-        self.data_root = 'dataset/lfw_cropped/split_data/test'
+        self.data_root = 'dataset/lfw_cropped/split_data/train'
         self.datasets = [LfwDataset(os.path.join(self.data_root, f'0{i}'))
                          for i in tqdm.tqdm(range(1, 11), desc='loading data...')]
         self.counter = []
         self.loss_history = []
         self.iteration_number = 0
         self.loss_func = nn.CrossEntropyLoss()
+
+    @staticmethod
+    def set_seed(seed):
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.backends.cudnn.deterministic = True
 
     # separate for train dataset and valid dataset
     def get_10_fold_data(self, index):
@@ -40,7 +50,8 @@ class Trainer:
             valid, train = self.get_10_fold_data(k)
 
             print('*' * 25, 'Number ', k, ' fold', '*' * 25)
-            train_ls, valid_ls = self.train_k(valid, train, num_epochs, seed=k, fusion_method='append', log_path='log/output.xlsx')
+            train_ls, valid_ls = self.train_k(valid, train, num_epochs, seed=k, fusion_method="kronecker",
+                                              log_path='log/output.xlsx')
             train_loss_sum += train_ls[-1][0]
             valid_loss_sum += valid_ls[-1][0]
             train_acc_sum += train_ls[-1][1]
@@ -51,18 +62,22 @@ class Trainer:
               'valid_loss_sum:%.6f' % (valid_loss_sum / 10), 'valid_acc_sum:%.6f' % (valid_acc_sum / 10))
 
     def train_k(self, valid_dataset, train_dataset, train_num_epochs, seed, fusion_method, log_path):
-        '''if not os.path.isfile(log_path):
+        self.set_seed(seed)
+        if not os.path.isfile(log_path):
             df = pd.DataFrame(columns=["Fold number", "Training epoch",
                                        "train_loss", "train_acc",
                                        "valid_loss", "valid_acc"])
-            df.to_excel(log_path)'''
+            df.to_excel(log_path)
 
         train_ls, valid_ls = [], []
-        torch.manual_seed(seed)
         model = self.model(fusion_method=fusion_method).to(self.device)
         optimizer = self.optimizer(model.parameters(), lr=self.lr, betas=(0.9, 0.98))
-        train_dataloader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
-        valid_dataloader = DataLoader(valid_dataset, batch_size=self.batch_size, shuffle=True)
+        train_dataloader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True,
+                                      num_workers=8,
+                                      pin_memory=True)
+        valid_dataloader = DataLoader(valid_dataset, batch_size=self.batch_size, shuffle=True,
+                                      num_workers=8,
+                                      pin_memory=True)
         model.train()
         for epoch in range(train_num_epochs):
             with tqdm.tqdm(enumerate(train_dataloader, 0),
@@ -71,27 +86,31 @@ class Trainer:
                 for i, data in t:
                     img1, img2, soft_bio1, soft_bio2, train_label = data
                     img1, img2, soft_bio1, soft_bio2, train_label = img1.to(self.device), img2.to(self.device), \
-                                                                    soft_bio1.to(self.device), soft_bio2.to(self.device), \
+                                                                    soft_bio1.to(self.device), soft_bio2.to(
+                        self.device), \
                                                                     train_label.to(self.device)
-
                     output1, output2 = model(img1, img2, soft_bio1, soft_bio2)
                     loss_contrastive = self.loss_fcn(output1, output2, train_label)
                     optimizer.zero_grad()
                     loss_contrastive.backward()
                     optimizer.step()
+                    #print(loss_contrastive.item())
                     train_dist = F.pairwise_distance(output1, output2)
                     train_predict_lb = (train_dist < 1).int()
                     train_accurancy = (train_predict_lb == train_label).float().mean().item()
                     train_ls.append(self.res(loss_contrastive.item(), train_accurancy))
             print('train_loss:%.6f' % train_ls[-1][0], 'train_acc:%.6f\n' % train_ls[-1][1])
-            '''log = {
+            log = {
                 "Fold number": seed,
                 "Training epoch": epoch,
-                "train_loss": f'{train_ls[-1][0]}:.4f',
-                "train_acc":,
-                "valid_loss":,
-                "valid_acc":
-            }'''
+                "train_loss": f'{train_ls[-1][0]}',
+                "train_acc": f'{train_ls[-1][1]}',
+                "valid_loss": "Null",
+                "valid_acc": "Null"
+            }
+            df = pd.read_excel(log_path, index_col=0)
+            df = df.append([log], ignore_index=True)
+            df.to_excel(log_path)
 
             count = 0
         with torch.no_grad():
@@ -102,10 +121,11 @@ class Trainer:
                 for j, data1 in t:
                     img01, img02, soft_bio01, soft_bio02, valid_label = data1
                     img01, img02, soft_bio01, soft_bio02, valid_label = img01.to(self.device), img02.to(self.device), \
-                                                                    soft_bio01.to(self.device), soft_bio02.to(self.device), \
-                                                                    valid_label.to(self.device)
+                                                                        soft_bio01.to(self.device), soft_bio02.to(
+                        self.device), \
+                                                                        valid_label.to(self.device)
 
-                    output01, output02 = model(img01, img02, soft_bio1, soft_bio2)
+                    output01, output02 = model(img01, img02, soft_bio01, soft_bio02)
                     loss_contrastive = self.loss_fcn(output01, output02, valid_label)
                     valid_dist = F.pairwise_distance(output01, output02)
                     valid_predict_lb = (valid_dist < 1).int()
@@ -113,7 +133,18 @@ class Trainer:
                     valid_accurancy = count.item() / len(valid_dataloader.dataset)
                     valid_ls.append(self.res(loss_contrastive.item(), valid_accurancy))
             print('valid_loss:%.6f' % valid_ls[-1][0], 'valid_acc:%.6f' % valid_ls[-1][1])
-        torch.save(model.state_dict(), f'model_state_dict/with_fusion/fusion_simple_append/model_{train_number_epochs}_{seed}.pkl')
+            log = {
+                "Fold number": seed,
+                "Training epoch": epoch,
+                "train_loss": "Null",
+                "train_acc": "Null",
+                "valid_loss": f'{valid_ls[-1][0]}',
+                "valid_acc": f'{valid_ls[-1][1]}'
+            }
+            df = pd.read_excel(log_path, index_col=0)
+            df = df.append([log], ignore_index=True)
+            df.to_excel(log_path)
+        torch.save(model.state_dict(), f'model_state_dict/with_fusion/100-k/model_{train_number_epochs}_{seed}.pkl')
         return train_ls, valid_ls
 
     def res(self, x, y):
@@ -124,9 +155,11 @@ class Trainer:
 def test_model(model, test_loader: DataLoader):
     model.eval()
     count = 0
-    for img1, img2, _, _, lb in test_loader:
-        img1, img2, lb = img1.to(t.device), img2.to(t.device), lb.to(t.device)
-        out1, out2 = model(img1, img2)
+    for img1, img2, soft_bio1, soft_bio2, lb in test_loader:
+        img1, img2, soft_bio1, soft_bio2, lb = img1.to(t.device), img2.to(t.device), \
+                                               soft_bio1.to(t.device), soft_bio2.to(t.device),\
+                                               lb.to(t.device)
+        out1, out2 = model(img1, img2, soft_bio1, soft_bio2)
         dist = F.pairwise_distance(out1, out2)
         p = (dist < 1).int()
         count += (p == lb).float().sum()
@@ -137,4 +170,4 @@ if __name__ == '__main__':
     train_number_epochs = 2
 
     t = Trainer(DSASN, learning_rate=2e-4, batch_size=32, use_cuda=True)
-    #t.k_fold_iteration(train_number_epochs)
+    t.k_fold_iteration(train_number_epochs)
